ff066cb6ebeab3e5e3eaae66feb83d5975728667
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-28 06:51:54 +0900

    fin Services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,6 +4,7 @@ import HeartEmoji from './../../img/heartemoji.png';
 import Glasses from './../../img/glasses.png';
 import Humble from './../../img/humble.png';
 import Card from './../Card/Card';
+import Resume from './office_worker_damgom.png';
 
 const Services = () => {
   return (
@@ -17,16 +18,40 @@ const Services = () => {
           <br />
           가나다라마바사아자차카타파하이찌니산시욘고로쿠시치나나하치큐쿠쥬
         </span>
-        <button className="button s-button">Download CV</button>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
         <div className="blur s-blur1" style={{ background: '#abf1ff94' }}></div>
       </div>
       {/* right side */}
       <div className="cards">
-        <Card
-          emoji={HeartEmoji}
-          heading={'Design'}
-          detail={'Figma, Sketch, Photoshop, Adobe, Adobe xd'}
-        />
+        <div style={{ left: '14rem' }}>
+          <Card
+            emoji={HeartEmoji}
+            heading={'Design'}
+            detail={'Figma, Sketch, Photoshop, Adobe, Adobe xd'}
+          />
+        </div>
+        <div style={{ top: '12rem', left: '-4rem' }}>
+          <Card
+            emoji={Glasses}
+            heading={'Developer'}
+            detail={'HTML, CSS, JavaScript, React'}
+          />
+        </div>
+        <div style={{ top: '19rem', left: '12rem' }}>
+          <Card
+            emoji={Humble}
+            heading={'UI/UX'}
+            detail={
+              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. '
+            }
+          />
+        </div>
+        <div
+          className="blur s-blur2"
+          style={{ background: 'var(--purple)' }}
+        ></div>
       </div>
     </div>
   );
