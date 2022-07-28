8a7b1c1b78700bc26698ff9130aa020e0a1a19fb
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-28 14:15:01 +0900

    working on darkmode

--- src/App.js
+++ src/App.js
@@ -8,10 +8,21 @@ import Portfolio from './components/Portfolio/Portfolio';
 import Testimonials from './components/Testimonials/Testimonials';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import { themeContext } from './Context';
+import { useContext } from 'react';
 
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        background: darkMode ? 'black' : '',
+        color: darkMode ? 'white' : '',
+      }}
+    >
       <Navbar />
       <Intro />
       <Services />
